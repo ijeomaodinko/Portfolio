cf70e00b41fd6da2a14fdf7a14991b578111c924
Ijeoma Odinko <95123554+ijeomaodinko@users.noreply.github.com>
2023-01-03 15:45:00 -0500

    Update about.jsx

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -10,7 +10,7 @@ function Aboutpage() {
                 <div className="aboutme">
                 <h1 className="h1"> About Myself</h1>
                 <p className="p">  Hello, I’m a software developer! I can help you build a product ,
-            feature or website Look through some of my work and experience! If
+            feature or website. Look through some of my work and experience! If
             you like what you see and have a project you need coded, don’t
             hestiate to contact me.</p>
             <p class="let-connect">
