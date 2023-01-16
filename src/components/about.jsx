d228a05b41022c2f7f54c9c55db62f438d5f940f
Ijeoma Odinko <95123554+ijeomaodinko@users.noreply.github.com>
2023-01-16 15:29:45 +0100

    Update about.jsx

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -19,27 +19,32 @@ function Aboutpage() {
                 </div>
                 <div className="contact">
                      <div>
-                     <a href="https://twitter.com/pureblog0">
+                     <a href="https://twitter.com/pureblog0" target="_blank"
+        rel="noreferrer"  >
                         < FaTwitter size={29} style={{ color: "#ccd6f6", marginRight:"1.9rem"}} />
                         </a>
                     </div>
                     <div>
-                    <a href="https://www.linkedin.com/in/ijeoma-odinko/">
+                    <a href="https://www.linkedin.com/in/ijeoma-odinko/" target="_blank"
+        rel="noreferrer" >
                         < FaLinkedin size={29} style={{ color: "#ccd6f6", marginRight:"1.9rem"}} />
                      </a>
                     </div>
                     <div>
-                    <a href="https://github.com/ijeomaodinko/">
+                    <a href="https://github.com/ijeomaodinko/"  target="_blank"
+        rel="noreferrer" >
                         < FaGithub size={29} style={{ color: "#ccd6f6", marginRight:"1.9rem"}} />
                     </a>
                     </div>
                     <div>
-                    <a href="https://medium.com/@estherlinei">
+                    <a href="https://medium.com/@estherlinei"  target="_blank"
+        rel="noreferrer" >
                         < FaMedium size={29} style={{ color: "#ccd6f6", marginRight:"1.9rem"}} />
                     </a>
                     </div>
                     <div>
-                     <a href="https://angel.co/u/ijeoma-odinko">
+                     <a href="https://angel.co/u/ijeoma-odinko" target="_blank"
+        rel="noreferrer" >
                         < FaAngellist size={29} style={{ color: "#ccd6f6"}} />
                         </a>
                     </div>
@@ -48,27 +53,32 @@ function Aboutpage() {
 
                 <div className="contact">
                      <div>
-                     <a href="https://twitter.com/pureblog0">
+                     <a href="https://twitter.com/pureblog0" target="_blank"
+        rel="noreferrer" >
                         < FaTwitter size={29} style={{ color: "#ccd6f6", marginRight:"1.9rem"}} />
                         </a>
                     </div>
                     <div>
-                    <a href="https://www.linkedin.com/in/ijeoma-odinko/">
+                    <a href="https://www.linkedin.com/in/ijeoma-odinko/"  target="_blank"
+        rel="noreferrer" >
                         < FaLinkedin size={29} style={{ color: "#ccd6f6", marginRight:"1.9rem"}} />
                      </a>
                     </div>
                     <div>
-                    <a href="https://github.com/ijeomaodinko/">
+                    <a href="https://github.com/ijeomaodinko/" target="_blank"
+        rel="noreferrer"  >
                         < FaGithub size={29} style={{ color: "#ccd6f6", marginRight:"1.9rem"}} />
                     </a>
                     </div>
                     <div>
-                    <a href="https://medium.com/@estherlinei">
+                    <a href="https://medium.com/@estherlinei" target="_blank"
+        rel="noreferrer"  >
                         < FaMedium size={29} style={{ color: "#ccd6f6", marginRight:"1.9rem"}} />
                     </a>
                     </div>
                     <div>
-                     <a href="https://angel.co/u/ijeoma-odinko">
+                     <a href="https://angel.co/u/ijeoma-odinko"  target="_blank"
+        rel="noreferrer" >
                         < FaAngellist size={29} style={{ color: "#ccd6f6"}} />
                         </a>
                     </div>
@@ -77,27 +87,32 @@ function Aboutpage() {
 
                 <div className="contacte">
                      <div>
-                     <a href="https://twitter.com/pureblog0">
+                     <a href="https://twitter.com/pureblog0" target="_blank"
+        rel="noreferrer" >
                         < FaTwitter size={29} style={{ color: "#ccd6f6", marginRight:"1.9rem"}} />
                         </a>
                     </div>
                     <div>
-                    <a href="https://www.linkedin.com/in/ijeoma-odinko/">
+                    <a href="https://www.linkedin.com/in/ijeoma-odinko/" target="_blank"
+        rel="noreferrer" >
                         < FaLinkedin size={29} style={{ color: "#ccd6f6", marginRight:"1.9rem"}} />
                      </a>
                     </div>
                     <div>
-                    <a href="https://github.com/ijeomaodinko/">
+                    <a href="https://github.com/ijeomaodinko/" target="_blank"
+        rel="noreferrer" >
                         < FaGithub size={29} style={{ color: "#ccd6f6", marginRight:"1.9rem"}} />
                     </a>
                     </div>
                     <div>
-                    <a href="https://medium.com/@estherlinei">
+                    <a href="https://medium.com/@estherlinei"  target="_blank"
+        rel="noreferrer" >
                         < FaMedium size={29} style={{ color: "#ccd6f6", marginRight:"1.9rem"}} />
                     </a>
                     </div>
-                    <div>
-                     <a href="https://angel.co/u/ijeoma-odinko">
+                    <div> 
+                     <a href="https://angel.co/u/ijeoma-odinko" target="_blank"
+        rel="noreferrer" >
                         < FaAngellist size={29} style={{ color: "#ccd6f6"}} />
                         </a>
                     </div>
@@ -105,7 +120,8 @@ function Aboutpage() {
                 </div>
 
                 <div className="resume">
-                <a href="https://drive.google.com/file/d/1p0MsdE1aLPb6er4fy-3XeW23N5FVcGK3/view?usp=sharing">
+                <a href="https://drive.google.com/file/d/1p0MsdE1aLPb6er4fy-3XeW23N5FVcGK3/view?usp=sharing"  target="_blank"
+        rel="noreferrer" >
                 Get My Resume </a></div>
             </div>
 
