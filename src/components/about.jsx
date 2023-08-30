12f05b692c01b8de7d19166afc60c33a1a83d479
ijeomaodinko <ijeomaodinko@gmail.com>
2023-08-30 11:40:06 +0100

    Add files

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FaTwitter, FaLinkedin, FaGithub, FaMedium, FaAngellist } from "react-icons/fa";
 import "./about.css";
-// import imgijeoma from "../assets/imgijeoma.jpg"
+import imgijeoma from "../assets/imgijeoma.png"
 
 function Aboutpage() {
     return (
@@ -92,11 +92,12 @@ function Aboutpage() {
                 Get My Resume </a></div>
             </div>
 
-            {/* <div className="imgie">
+            <div className="imgie">
             <div className="imgportfolio">
                 <img  className="aboutimg" src={imgijeoma}  alt= "Ijeoma" />
             </div>
-            </div> */}
+            </div>
+            
             </div>
         </div>
     )
